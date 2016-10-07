b0188fd9bd1f092c9105973d4fa62afa59d9086f
Mads <Mads@Scientia.eduroam.wireless.dtu.dk>
2016-10-07 09:55:09 +0200

    Kommentarer

--- Dice/terning-master/Dice.java
+++ Dice/terning-master/Dice.java
@@ -5,9 +5,9 @@ public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
 		float d1=(float)Math.random();     // 0-1
-		float d2=d1*8;                     // 0-7 Ændring fra 7 til 8.
-		int d3 = (int) d2;             // 0-7 integer der runder ned.
-		return d3 + 1;                     // 1-8		
+		float d2=d1*6;                     // 0-6 Ændring fra 5 til 6.
+		int d3 = (int) d2;             // 0-5 integer der runder ned.
+		return d3 + 1;                     // 1-6		
 	}
 	
 	// roll the die n times and print the values
